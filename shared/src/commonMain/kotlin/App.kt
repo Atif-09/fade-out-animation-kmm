151aa0ae23d0cb33590385aa8c00d6f12aa927d0
Atif-09 <ma2669637@gmail.com>
2023-07-29 18:33:25 +0500

    Initial Commit

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,3 +1,4 @@
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -11,6 +12,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -18,30 +23,36 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import model.IslandDataClass
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.resource
+import kotlin.math.absoluteValue
 
-@OptIn(ExperimentalResourceApi::class)
+@OptIn(ExperimentalResourceApi::class, ExperimentalFoundationApi::class)
 @Composable
 fun App() {
     MaterialTheme {
         val listOfIsland = listOf(
-            IslandDataClass(1, "Bali", "Indonesia", Color(0xFF3389e2), "bali_icon.jpg"),
-            IslandDataClass(2, "Yosemite", "California", Color(0xFFee6659), "yosemite.jpg"),
-            IslandDataClass(3, "Rayyu", "Maldives", Color(0xFF4ecab0), "rayyu_maldives.jpg"),
-            IslandDataClass(4, "Viti Levu", "Fiji", Color(0xFF7D5260), "viti_levu.jpg"),
-            IslandDataClass(5, "Beqa", "Fiji", Color(0xFF625b71), "beqa.jpg")
+            IslandDataClass(1, "pic1.jpg"),
+            IslandDataClass(2, "pic2.jpg"),
+            IslandDataClass(3, "pic3.jpg"),
+            IslandDataClass(4,  "pic4.jpg"),
+            IslandDataClass(5, "pic5.jpg"),
+            IslandDataClass(6, "pic6.jpg"),
+            IslandDataClass(7, "pic7.jpg")
         )
         val bitMapImages = remember { mutableStateOf<Map<Int, ImageBitmap>>(emptyMap()) }
+
 
         LaunchedEffect(Unit) {
             val imagesMap = mutableMapOf<Int, ImageBitmap>()
@@ -52,12 +63,55 @@ fun App() {
         }
 
 
-        Column(modifier = Modifier.padding(top = 12.dp, bottom = 12.dp)) {
-            ItemList(listOfIsland, bitMapImages.value)
+
+
+
+        Column(modifier = Modifier.padding(top = 18.dp, bottom = 18.dp, start = 12.dp, end = 12.dp)) {
+            FadeAnimation(listOfIsland, bitMapImages.value)
+        }
+    }
+}
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun FadeAnimation(itemList: List<IslandDataClass>, image: Map<Int, ImageBitmap>) {
+    val pagerState = rememberPagerState()
+
+    HorizontalPager(
+        pageCount = itemList.size,
+        state = pagerState,
+        pageSize = PageSize.Fill
+    ) { index ->
+
+        Box(
+            modifier = Modifier.fillMaxSize()
+                .graphicsLayer {
+                    val pageOffset = pagerState.calculateCurrentOffsetForPage(index)
+                    // translate the contents by the size of the page, to prevent the pages from sliding in from left or right and stays in the center
+                    translationX = pageOffset * size.width
+                    // apply an alpha to fade the current page in and the old page out
+                    alpha = 1 - pageOffset.absoluteValue
+                }
+        ) {
+            image[itemList[index].id]?.let {
+                Image(
+                    bitmap = it, // Retrieve the ImageBitmap from the map using the item's id as the key
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize().clip(RoundedCornerShape(30.dp)),
+                )
+            }
+
         }
     }
 }
 
+// extension method for current page offset
+@OptIn(ExperimentalFoundationApi::class)
+fun PagerState.calculateCurrentOffsetForPage(page: Int): Float {
+    return (currentPage - page) + currentPageOffsetFraction
+}
+
+/*
 @Composable
 fun ItemList(itemList: List<IslandDataClass>, image: Map<Int, ImageBitmap>) {
     LazyColumn {
@@ -67,7 +121,9 @@ fun ItemList(itemList: List<IslandDataClass>, image: Map<Int, ImageBitmap>) {
         }
     }
 }
+*/
 
+/*
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun CardImageView(item: IslandDataClass, image: ImageBitmap) {
@@ -125,5 +181,6 @@ fun CardImageView(item: IslandDataClass, image: ImageBitmap) {
         )
     }
 }
+*/
 
 expect fun getPlatformName(): String
--- shared/src/commonMain/kotlin/model/IslandDataClass.kt
+++ shared/src/commonMain/kotlin/model/IslandDataClass.kt
@@ -4,8 +4,5 @@ import androidx.compose.ui.graphics.Color
 
 data class IslandDataClass(
     val id: Int,
-    val name: String,
-    val country: String,
-    val color: Color,
     val resImage: String
 )
